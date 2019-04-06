48a059afa105aba9307a3295a024da2fd6074435
lilili001 <2861166132@qq.com>
2019-04-06 18:30:45 +0800

    style样式的三种写法

--- my-app/src/components/TodoItem.js
+++ my-app/src/components/TodoItem.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 class TodoItem extends Component {
+    getStyle=()=>{
+        if(this.props.todo.completed){
+            return {
+                textDecoration:'line-through'
+            }
+        }else{
+            return {
+                textDecoration:'none'
+            }
+        }
+    }
   render() {
     return (
-        <div>{this.props.todo.title}</div>
-    );
+        <div>
+        <div style={{backgroundColor:'#f4f4f4'}}>{this.props.todo.title}</div>
+        <div style={itemStyle}>{this.props.todo.title}</div>
+        <div style={this.getStyle()}>{this.props.todo.title}</div>
+        </div>
+     );
   }
 }
 TodoItem.propTypes={
   todo:PropTypes.object.isRequired
 }
+
+const itemStyle = {
+  backgroundColor:'#5f5f5'
+}
+
 export default TodoItem;
