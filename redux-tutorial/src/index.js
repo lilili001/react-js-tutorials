a408532949a1720edd8c19a8d8adae3fefd216b7
lilili001 <2861166132@qq.com>
2019-04-14 08:27:51 +0800

    redux 初始化state

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -15,7 +15,10 @@ const allReducers = combineReducers({
     products:productReducer,
     user:userReducer
 })
-const store = createStore(allReducers);
+const store = createStore(allReducers,{
+    products:[{name:'iPhone'}],
+    user:'Michael'
+});
 console.log(store.getState())
 ReactDOM.render(<App />, document.getElementById('root'));
 
