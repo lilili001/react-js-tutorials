91066c0690d02bbf96868c41f5aada812089aa4f
lilili001 <2861166132@qq.com>
2019-04-14 08:20:41 +0800

    redux basic

---
+++ redux-tutorial/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import {createStore} from 'redux'
+
+function reducer(state,action) {
+    if(action.type == 'changeState'){
+        return action.payload
+    }
+    return 'state111'
+}
+const store = createStore(reducer);
+
+console.log(store.getState())
+
+const action = {type:'changeState',payload:{newState:'New State'}}
+store.dispatch(action)
+console.log(store.getState());
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
