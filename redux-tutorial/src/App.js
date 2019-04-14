3d4214ab1667d197c71576ab07200064b82a6d24
lilili001 <2861166132@qq.com>
2019-04-14 09:49:00 +0800

    mapStateToProps,mapActionsToProps

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
+
+import {updateUser} from './actons/userAction'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.updateUser = this.updateUser.bind(this)
+    }
+
+    updateUser(event) {
+        console.log(111)
+        this.props.onUpdateUser(event.target.value);
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+
+                    <input onChange={this.updateUser}/>
+                    {this.props.user}
+                </header>
+
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    products: state.products,
+    user: state.user
+})
+
+//mapActionsToProps是一个对象 其中的actions自动emit
+const mapActionsToPros = {
+    onUpdateUser: updateUser
+}
+export default connect(mapStateToProps, mapActionsToPros)(App);
--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -3,32 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {combineReducers, createStore} from 'redux'
 import {Provider} from 'react-redux'
-
-function productReducer(state = [], action) {
-    return state
-}
-function userReducer(state = '', {type,payload}) {
-    switch (type){
-        case 'updateUser':
-            return payload
-    }
-    return state
-}
-const allReducers = combineReducers({
-    products: productReducer,
-    user: userReducer
-})
-const store = createStore(
-    allReducers,
-    {
-        products: [{name: 'iPhone'}],
-        user: 'Michael'
-    },
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-console.log(store.getState())
+import store from './store';
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
