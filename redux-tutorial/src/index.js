c6f115b1bd097a6dcb996a3bfb2344b7e08a5a9c
lilili001 <2861166132@qq.com>
2019-04-14 09:07:43 +0800

    引入provider

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 import {combineReducers, createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 function productReducer(state = [], action) {
     return state
@@ -29,14 +30,8 @@ const store = createStore(
 );
 console.log(store.getState())
 
-const updateUserAction = {
-    type:'updateUser',
-    payload:{
-        user:'John'
-    }
-}
-store.dispatch(updateUserAction)
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
