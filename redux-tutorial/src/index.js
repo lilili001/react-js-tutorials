a6a4a1148cb757bca0bc456c02aad372ab5f9305
lilili001 <2861166132@qq.com>
2019-04-14 08:25:52 +0800

    redux combineReducers

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -2,26 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux'
+import {combineReducers,createStore} from 'redux'
 
-function reducer(state,action) {
-    if(action.type == 'changeState'){
-        return action.payload
-    }
-    return 'state111'
+function productReducer(state = [],action) {
+    return state
 }
-const store = createStore(reducer);
-
+function userReducer(state = '',action) {
+    return state
+}
+const allReducers = combineReducers({
+    products:productReducer,
+    user:userReducer
+})
+const store = createStore(allReducers);
 console.log(store.getState())
-
-const action = {type:'changeState',payload:{newState:'New State'}}
-store.dispatch(action)
-console.log(store.getState());
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
